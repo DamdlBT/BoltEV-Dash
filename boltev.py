7271abc25ade1969bb962ecc6b07a01fbccf18ce
DamdlBT <56141589+DamdlBT@users.noreply.github.com>
2019-10-14 11:17:31 -0400

    Ajout callback

--- boltev.py
+++ boltev.py
@@ -1,16 +1,23 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
 
 layout = html.Div(
     children=[
-        html.H1("Bolt EV - Efficacité et Autonomie")
+        html.H1("Bolt EV - Efficacité et Autonomie"),
+        dcc.Input(id="in"),
+        html.P(id="out"),
     ]
 )
 
 app = dash.Dash(__name__)
 
 app.layout = layout
+
+@app.callback(Output("out", "children"), [Input("in", "value")])
+def update_output(in_value):
+    return in_value
 
 if __name__ == '__main__':
 

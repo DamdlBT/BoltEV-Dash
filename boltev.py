685446404801f556194c010c3851a965792471f5
DamdlBT <56141589+DamdlBT@users.noreply.github.com>
2019-10-14 13:32:10 -0400

    Ajout CSS et mise en page

--- boltev.py
+++ boltev.py
@@ -10,32 +10,52 @@ days = pd.read_csv("data/data.csv")
 layout = html.Div(
     children=[
         html.H1("Bolt EV - Efficacité et Autonomie"),
-        html.Label('Variable x'),
-        dcc.Dropdown(
-            id="x",
-            options=[
-                {"label": c, "value": c} for c in sorted(days.columns)
+        html.Hr(),
+        html.Div(
+            children=[
+                html.Label('Variable x'),
+                dcc.Dropdown(
+                    id="x",
+                    options=[
+                        {"label": c, "value": c} for c in sorted(days.columns)
+                    ],
+                    placeholder="choisir la variable",
+                ),
+                html.Label('Variable y'),
+                dcc.Dropdown(
+                    id="y",
+                    options=[
+                        {"label": c, "value": c} for c in sorted(days.columns)
+                    ],
+                    placeholder="choisir la variable",
+                ),
+                html.Label('Variable couleur'),
+                dcc.Dropdown(
+                    id="couleur",
+                    options=[
+                        {"label": c, "value": c} for c in sorted(days.columns)
+                    ],
+                    placeholder="choisir la variable",
+                ),
             ],
-            placeholder="choisir la variable",
+            style={"width": "25%", "display": "inline-block", "float": "left" }
         ),
-        html.Label('Variable y'),
-        dcc.Dropdown(
-            id="y",
-            options=[
-                {"label": c, "value": c} for c in sorted(days.columns)
+        html.Div(
+            children=[
+                dcc.Graph(id="graph")
             ],
-            placeholder="choisir la variable",
+            style={"width": "75%", "display": "inline-block",}
         ),
-        html.Label('Variable couleur'),
-        dcc.Dropdown(
-            id="couleur",
-            options=[
-                {"label": c, "value": c} for c in sorted(days.columns)
+        html.Hr(),
+        html.Div(
+            children=[
+                html.Div(["Démo d'application utilisant ", html.A("Plotly Dash", href="https://plot.ly/dash/"), " par Damien de la Bruère-Terreault."]),
+                html.A("Code source sur GitHub",  href="https://github.com/DamdlBT/BoltEV-Dash"),
             ],
-            placeholder="choisir la variable",
-        ),
-        dcc.Graph(id="graph")
-    ]
+            style={"text-align": "center"}
+        )
+    ],
+    style={"max-width": "1200px", "margin": "auto"}
 )
 
 app = dash.Dash(__name__)

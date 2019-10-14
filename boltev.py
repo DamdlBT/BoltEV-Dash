81d9ec8244361a7d2f8b5e91bd26520f969dcfe2
DamdlBT <56141589+DamdlBT@users.noreply.github.com>
2019-10-14 11:37:25 -0400

    Ajout des données et menus déroulants

--- boltev.py
+++ boltev.py
@@ -2,22 +2,35 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+import  pandas as pd
+
+days = pd.read_csv("data/data.csv")
 
 layout = html.Div(
     children=[
         html.H1("Bolt EV - Efficacité et Autonomie"),
-        dcc.Input(id="in"),
-        html.P(id="out"),
+        html.Label('Variable x'),
+        dcc.Dropdown(
+            id="x",
+            options=[
+                {"label": c, "value": c} for c in sorted(days.columns)
+            ],
+            placeholder="choisir la variable",
+        ),
+        html.Label('Variable y'),
+        dcc.Dropdown(
+            id="y",
+            options=[
+                {"label": c, "value": c} for c in sorted(days.columns)
+            ],
+            placeholder="choisir la variable",
+        ),
     ]
 )
 
 app = dash.Dash(__name__)
 
 app.layout = layout
-
-@app.callback(Output("out", "children"), [Input("in", "value")])
-def update_output(in_value):
-    return in_value
 
 if __name__ == '__main__':
 
